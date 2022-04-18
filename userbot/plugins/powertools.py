bd8d47f26e9e35ffcfc5bda1d590216a7fc1e68e
mJustUnique <94372313+mJustUnique@users.noreply.github.com>
2022-04-18 11:06:25 +0530

    Update powertools.py

--- userbot/plugins/powertools.py
+++ userbot/plugins/powertools.py
@@ -30,10 +30,10 @@ plugin_category = "tools"
 async def _(event):
     "Restarts the bot !!"
     if BOTLOG:
-        await event.client.send_message(BOTLOG_CHATID, "#RESTART \n" "Bot Restarted...")
+        await event.client.send_message(BOTLOG_CHATID, "#RESTART \n" "Bot Restarted !!")
     sandy = await edit_or_reply(
         event,
-        "__**Restarted...**__ `.ping` me or `.help` to check if I am online, actually it takes 1-2 min for restarting.",
+        "**Restarted...** `.ping` me or `.help` to check if I am online, actually it takes 1-2 min for restarting.",
     )
     try:
         ulist = get_collectionlist_items()
